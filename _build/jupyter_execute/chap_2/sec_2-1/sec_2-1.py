22f69997dab41b9e6feb0fbe57357d8b945311bb
narizuka <pararel@gmail.com>
2022-10-16 15:22:52 +0900

    updates

--- _build/jupyter_execute/chap_2/sec_2-1/sec_2-1.py
+++ _build/jupyter_execute/chap_2/sec_2-1/sec_2-1.py
@@ -235,7 +235,7 @@ fig.savefig('./box_ex.png', bbox_inches="tight",\
 
 # **Pythonでの実装**
 # 
-# 具体例として，ここではIris Dataset（[Kaggleのウェブサイト](https://www.kaggle.com/uciml/iris)からダウンロード可能）に含まれるアヤメのがく片の長さ（Sepal Length），がく片の幅（Sepal Width），花弁の長さ（Petal Length），花弁の幅（Petal Width）のデータを用いる．
+# 具体例として，ここでは[Iris Dataset](https://drive.google.com/uc?export=download&id=1dmRM6SiafHEkCH_c7G0KHmt_IISqKrWN)に含まれるアヤメのがく片の長さ（Sepal Length），がく片の幅（Sepal Width），花弁の長さ（Petal Length），花弁の幅（Petal Width）のデータを用いる．
 # まずはIrisデータセットをダウンロードして作業フォルダに保存し，以下のようにPandasのDataFrame形式で読み込む：
 
 # In[3]:
